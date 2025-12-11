485f5e39f58383e3803857def8bc7bd5aa262f03
Alexis GOUMENT <alexis.nguyen.goument@gmail.com>
2025-12-11 22:23:23 +0100

    Day1 decoy

---
+++ AdventCode2025.Tests/Day1.Tests.fs
@@ -0,0 +1,68 @@
+﻿module Day1.Tests
+
+open System
+open Xunit
+open Day1
+
+let ``given dialPosition is`` dialPosition =
+    dialPosition
+
+let ``when operateSafe`` (instruction: string) dialPosition =
+    operateSafe dialPosition instruction
+
+let ``then dialPosition should be`` (expected: int) (actual: int) =
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Rotate L0`` () =
+    ``given dialPosition is`` 50
+    |> ``when operateSafe`` "L0"
+    |> ``then dialPosition should be`` 50
+
+[<Fact>]
+let ``Rotate R0`` () =
+    ``given dialPosition is`` 50
+    |> ``when operateSafe`` "R0"
+    |> ``then dialPosition should be`` 50
+
+[<Fact>]
+let ``Rotate L1`` () =
+    ``given dialPosition is`` 50
+    |> ``when operateSafe`` "L1"
+    |> ``then dialPosition should be`` 49
+
+[<Fact>]
+let ``Rotate R1`` () =
+    ``given dialPosition is`` 50
+    |> ``when operateSafe`` "R1"
+    |> ``then dialPosition should be`` 51
+
+[<Fact>]
+let ``Rotate L51`` () =
+    ``given dialPosition is`` 50
+    |> ``when operateSafe`` "L51"
+    |> ``then dialPosition should be`` 99
+
+[<Fact>]
+let ``Rotate L50`` () =
+    ``given dialPosition is`` 50
+    |> ``when operateSafe`` "L50"
+    |> ``then dialPosition should be`` 0
+
+[<Fact>]
+let ``Rotate R50`` () =
+    ``given dialPosition is`` 50
+    |> ``when operateSafe`` "R50"
+    |> ``then dialPosition should be`` 0
+
+[<Fact>]
+let ``Rotate R51`` () =
+    ``given dialPosition is`` 50
+    |> ``when operateSafe`` "R51"
+    |> ``then dialPosition should be`` 1
+
+[<Fact>]
+let ``Open safe with multiple instructions`` () =
+    let instructions = [ "R10"; "L20"; "R30"; "L40" ]
+    let finalPosition = openSafe instructions
+    Assert.Equal(30, finalPosition)
---
+++ AdventCode2025/Day1.fs
@@ -0,0 +1,38 @@
+module Day1
+
+// let openSafe instructions
+
+type Instruction = 
+| Left of int
+| Right of int
+
+let (|Instruction|_|) (str: string) =
+    let convertToInt chars = int (System.String(List.toArray chars))
+    match Seq.toList str with
+    | 'L'::rest -> Some (Left (convertToInt rest))
+    | 'R'::rest -> Some (Right (convertToInt rest))
+    | _ -> None
+
+let increment position n = 
+    let newPosition = position + n
+    if newPosition >= 100 then
+        newPosition - 100
+    else
+        newPosition
+
+let decrement position n = 
+    let newPosition = position - n
+    if newPosition < 0 then
+        100 + newPosition
+    else
+        newPosition
+
+let operateSafe dialPosition (instruction:string) =
+    match instruction with
+    | Instruction (Left nb) -> decrement dialPosition nb
+    | Instruction (Right nb) -> increment dialPosition nb
+    | _ -> failwithf "Invalid instruction: %s" instruction
+
+let openSafe (instructions: string list) =
+    instructions
+    |> List.fold operateSafe 50
